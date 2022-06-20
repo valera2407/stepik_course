cbe92d27b8186ad7b5366951b366e0f253c648d0
valera2407 <86014651+valera2407@users.noreply.github.com>
2022-06-20 13:51:39 +0300

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -5,7 +5,7 @@ from selenium.webdriver.common.by import By
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
-def test_guest_should_see_login_link(browser):
+def test_busket_button_has_different_language(browser):
     browser.get(link)
     time.sleep(10)
     button = browser.find_element(By.CSS_SELECTOR, ".btn-add-to-basket")
